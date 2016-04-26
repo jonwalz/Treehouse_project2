53235f90396f5230d0de72c80b56ff85fd25f436
Jonathan Walz <jon.walz.music@gmail.com>
2016-04-26 11:04:02 -0700

    Fixed nav on desktop

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,16 @@
 var gulp = require('gulp');
 var livereload = require('gulp-livereload');
+var cssmin = require('gulp-cssmin');
+var rename = require('gulp-rename');
 
-
-// gulp.task('load', function() {
-//    gulp.src('index.html')
-//        .pipe(livereload({ start: true }));
-// });
+gulp.task('load', function() {
+   gulp.src('css/*.css')
+       .pipe(cssmin())
+       .pipe(rename({suffix: '.min'}))
+       .pipe(livereload({ start: true }));
+});
 
 gulp.task('watch', function () {
-    livereload.listen({
-      port: 1234,
-      basePath: 'index.html'
-   }
-   );
-    gulp.watch(['css/*.css', 'index.html']);
-});
+    livereload.listen({port: 1234});
+    gulp.watch('css/*.css', ['load']);
+}); 
