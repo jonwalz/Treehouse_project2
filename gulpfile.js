262ab5d3014bf29c181b81354140eab09160660e
Jonathan Walz <jon.walz.music@gmail.com>
2016-04-26 09:47:47 -0700

    Added gulp

--- gulpfile.js
+++ gulpfile.js
@@ -2,12 +2,16 @@ var gulp = require('gulp');
 var livereload = require('gulp-livereload');
 
 
-gulp.task('load', function() {
-   gulp.src('index.html')
-       .pipe(livereload({ start: true }));
-});
+// gulp.task('load', function() {
+//    gulp.src('index.html')
+//        .pipe(livereload({ start: true }));
+// });
 
 gulp.task('watch', function () {
-    livereload.listen();
-    gulp.watch(['css/*.css', 'index.html'], ['load']);
+    livereload.listen({
+      port: 1234,
+      basePath: 'index.html'
+   }
+   );
+    gulp.watch(['css/*.css', 'index.html']);
 });
